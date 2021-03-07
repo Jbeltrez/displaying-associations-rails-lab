b2fb2cc905acac0306570fe42c8c982403fd5543
Jbeltrez <beltrezjohn@gmail.com>
2021-03-06 21:33:49 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,8 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  #needs to have an #artist_name method that exists within this model 
+  def artist_name 
+    self.artist.name 
+  end
 end
